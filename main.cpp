4dc02fd8aa39e90764f816f9237c938045cd2c8d
cloudstudyJ <cloudstudyJ@gmail.com>
2024-11-27 01:53:10 +0900

    Abstracted ray-casting process

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ using std::cout;
 using std::endl;
 using std::ofstream;
 
+#include "world.hpp"
+#include "models/sphere.hpp"
 #include "utilities/camera.hpp"
 #include "utilities/ray.hpp"
-#include "objects/sphere.hpp"
-#include "math/solver.hpp"
 
 unsigned int imgW{ 512 };
 unsigned int imgH{ };
@@ -27,6 +27,17 @@ void writeHeader(ofstream& writer) {
     writer << imgW << ' ' << imgH << endl;
     writer << 255 << endl;
 }
+Vec3<int> calcColors(const World& world, const Ray& ray) {
+    RayHitResult hitResult = world.raycast(ray);
+    Vec3<float> color;
+
+    if (hitResult.isHit)
+        color = (((hitResult.normal + 1.0f) * 0.5f) * 255);
+    else
+        color = (Math::lerpf(whiteColor, skyBlueColor, (ray.dir().normalize().y + 1.0f) * 0.5f) * 255);
+
+    return color;
+}
 
 int main() {
     imgH = static_cast<int>(std::round(imgW / aspectRatio));
@@ -42,35 +53,23 @@ int main() {
     if (writer) {
         writeHeader(writer);
 
+        World world("Main");
+        world.addModel(new Sphere({ 0.0f, 0.0f, -1.0f }, 0.5f));
+
         Camera camera;
-        Sphere sphere({ 0.0f, 0.0f, -1.0f }, 0.5f);
 
         Vec3<float> viewportLTv = camera.pos() - Vec3<float>(0.0f, 0.0f, focalLength);
         viewportLTv -= (viewportXv / 2.0f);
         viewportLTv -= (viewportYv / 2.0f);
-
         Vec3<float> pixelLTv = viewportLTv + ((dPixelXv + dPixelYv) * 0.5f);
 
         // Render
-        Vec3<int> rayColor;
-        Ray ray(camera.pos(), { });
-
         for (int row{ }; row < imgH; ++row) {
             for (int col{ }; col < imgW; ++col) {
-                Vec3<float> currentPixelCenter = pixelLTv + ((dPixelXv * col) + (dPixelYv * row));
-                ray.dir(currentPixelCenter - camera.pos());
+                Vec3<float> curPixelCenter = (pixelLTv + ((dPixelXv * col) + (dPixelYv * row)));
+                Ray ray(camera.pos(), curPixelCenter - camera.pos());
 
-                Vec3<float> unitDir = ray.dir().normalize();
-                float t = Solver::rayCastToSphere(ray, sphere);
-
-                if (t >= 0.0f) {
-                    Vec3<float> normal = ((ray.pos() + (ray.dir() * t)) - sphere.pos()).normalize();
-
-                    rayColor = ((normal + 1.0f) * 0.5f) * 255;
-                }
-                else
-                    rayColor = Math::lerpf(whiteColor, skyBlueColor, (unitDir.y + 1.0f) * 0.5f) * 255;
-
+                Vec3<int> rayColor = calcColors(world, ray);
                 writer << rayColor.x << ' ' << rayColor.y << ' ' << rayColor.z;
                 writer << ((col != (imgW - 1)) ? ' ' : '\n');
             }
