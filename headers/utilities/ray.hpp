2ae724ebc97b0b71a52d38970478282d41b22a48
cloudstudyJ <cloudstudyJ@gmail.com>
2024-11-27 01:48:54 +0900

    Added RayHitResult struct

--- headers/utilities/ray.hpp
+++ headers/utilities/ray.hpp
@@ -2,6 +2,15 @@
 
 #include "math/vector/vec3.hpp"
 
+struct RayHitResult {
+    bool isHit{ };
+
+    float distance{ };
+
+    Vec3<float> contactPoint;
+    Vec3<float> normal;
+};
+
 class Ray {
         Ray() = delete;
         Ray(const Ray&) = delete;
@@ -14,10 +23,12 @@ class Ray {
         Ray(const Vec3<float>&, const Vec3<float>&) noexcept;
         ~Ray() noexcept;
 
+        // Setters
         void pos(const Vec3<float>&) noexcept;
         void dir(const Vec3<float>&) noexcept;
         void color(const Vec3<float>&) noexcept;
 
+        // Getters
         Vec3<float> pos() const noexcept;
         Vec3<float> dir() const noexcept;
         Vec3<float> color() const noexcept;
--- sources/utilities/ray.cpp
+++ sources/utilities/ray.cpp
@@ -4,10 +4,12 @@ Ray::Ray(const Vec3<float>& pos, const Vec3<float>& dir) noexcept
     : mPos{pos}, mDir{dir} { }
 Ray::~Ray() noexcept { }
 
+// Setters
 void Ray::pos(const Vec3<float>& p) noexcept { mPos = p; }
 void Ray::dir(const Vec3<float>& d) noexcept { mDir = d; }
 void Ray::color(const Vec3<float>& color) noexcept { mColor = color; }
 
+// Getters
 Vec3<float> Ray::pos() const noexcept { return mPos; }
 Vec3<float> Ray::dir() const noexcept { return mDir; }
 Vec3<float> Ray::color() const noexcept { return mColor; }
