994721d35a22a9e89609ba1998d1a11e062f740a
cloudstudyJ <cloudstudyJ@gmail.com>
2024-12-15 20:57:43 +0900

    Added range(min, max) function

--- headers/utilities/random.hpp
+++ headers/utilities/random.hpp
@@ -17,14 +17,29 @@ class Random {
         template <typename T, typename U1, typename U2>
         static inline Vec<T, 3> Vec3(const U1&, const U2&) noexcept;
 
+        template <typename T, typename U1, typename U2>
+        static inline T range(const U1&, const U2&) noexcept;
+
     private:
         static std::random_device rd;
-        static std::mt19937 gen;
+        static std::mt19937 engine;
 };
 
 template <typename T, typename U1, typename U2>
 inline Vec<T, 3> Random::Vec3(const U1& min, const U2& max) noexcept {
     std::uniform_real_distribution<T> dis(static_cast<T>(min), static_cast<T>(max));
 
-    return { dis(gen), dis(gen), dis(gen) };
+    return { dis(engine), dis(engine), dis(engine) };
+}
+
+template <typename T, typename U1, typename U2>
+inline T Random::range(const U1& min, const U2& max) noexcept {
+    using distribution = IF<
+        isInteger<T>,
+        std::uniform_int_distribution<T>,
+        std::uniform_real_distribution<T>
+    >;
+
+    distribution dis(static_cast<T>(min), static_cast<T>(max));
+    return dis(engine);
 }
--- sources/utilities/random.cpp
+++ sources/utilities/random.cpp
@@ -1,4 +1,4 @@
 #include "utilities/random.hpp"
 
 std::random_device Random::rd;
-std::mt19937 Random::gen(rd());
+std::mt19937 Random::engine(rd());
